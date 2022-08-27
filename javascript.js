30a442d3c26de49bb1450a0b0fdb40a0b6591dc5
Suryateja Sunkoju <sunkojusuryateja@gmail.com>
2022-08-27 17:56:30 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-var str=prompt("Paste the text that has to be Changed");
 function setCharAt(str, index, char)
 {
     return str.substring(0, index)+char+str.substring(index+1);
 }
 function changeSpacesToUnderScores(str)
 {
+    var str=prompt("Paste the text that has to be Changed");
   for(var i=0; i<str.length; i++)
   {
       console.log(str[i]===" ");
@@ -14,9 +14,7 @@ function changeSpacesToUnderScores(str)
           str=setCharAt(str, i, '_');
       }
   }
-  return str;
+  alert(str);
+  return "done";
 }
-str=changeSpacesToUnderScores(str);
 //window.print("output:- "+str); This command is printing the current page, a window got opened to save as pdf.
-alert("output is below :) ");
-alert(str);
